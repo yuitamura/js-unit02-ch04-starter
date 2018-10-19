77ed5d446ad413dab74d68143db5d3ab1d1c837b
Atsuhiro Teshima <hiro@lingualbox.com>
2018-10-20 00:54:36 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+class Character {
+  constructor() {
+  }
+
+  showStatus() {
+    /* 
+      キャラクターの名前、HP、MPを表示する。
+    */
+  }
+
+  attack(defender) {
+    /*
+      キャラクターが死んでいる場合は攻撃出来ないので、それを表示する。
+      死んでいない場合は相手に与えたダメージを表示。
+      相手が死んだ場合は相手に与えたダメージと死んだことを表示する。 
+    */
+  }
+
+  calcAttackDamage(defender) {
+    /*
+      ダメージは単純に攻撃力から防御力を引いて計算する。
+      ダメージが0未満の場合は、最低のダメージ1を与える。
+    */
+  }
+}
+
+class Sorcerer extends Character {
+  constructor() {
+    
+  }
+
+  healSpell(target) {
+    /* 
+      回復魔法は3のMPを消費する。
+      相手のHPを15回復する。
+      魔法使いが死んでいる場合はその旨を表示する。
+      相手が死んでいる場合は回復が出来ないためその旨を表示する。
+      MPが足りない場合はその旨を表示する。
+    */
+  }
+
+  fireSpell(target) {
+    /* 
+      攻撃魔法は2のMPを消費する。
+      相手に10のダメージを与える。
+      魔法使いが死んでいる場合はその旨を表示する。
+      相手が死んでいる場合は攻撃が出来ないためその旨を表示する。
+      MPが足りない場合はその旨を表示する。
+    */
+  }
+}
+
+{
+  const fighter = new Character({
+    name: '武道家',
+    hp: 40,
+    mp: 0,
+    offensePower: 15,
+    defencePower: 10
+  })
+  const sorcerer = new Sorcerer({
+    name: '魔法使い',
+    hp: 25,
+    mp: 10,
+    offensePower: 8,
+    defencePower: 10
+  })
+  const monster = new Character({
+    name: 'モンスター',
+    hp: 60,
+    mp: 0,
+    offensePower: 30,
+    defencePower: 10
+  })
+
+  fighter.attack(monster);
+  sorcerer.attack(monster);
+  monster.attack(sorcerer);
+  fighter.attack(monster);
+  sorcerer.healSpell(sorcerer);
+  monster.attack(fighter);
+  fighter.attack(monster);
+  sorcerer.fireSpell(monster);
+  monster.attack(fighter);
+  fighter.showStatus();
+  sorcerer.showStatus();
+  monster.showStatus();
+}
